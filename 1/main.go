4f8752cb900ef2d091fea278a0737439995ead98
ahadahamedakash <a.ahad2232@gmail.com>
2025-12-07 16:10:35 +0600

    first commit

---
+++ 1/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main(){
+	// INT
+	fmt.Println(1)
+
+	fmt.Println(1 + 1)
+
+	// STRING
+	fmt.Println("This is string!")
+
+	// FLOATS
+	fmt.Println(10.5)
+
+	// BOOLEAN
+	fmt.Println(true)
+
+}
---
+++ 2/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+func main(){
+	var name string = "golang"
+
+	var name_infer = "Golang"
+
+	// SHORTHAND SYNTAX
+	name_two := "Golang"
+
+	var age int = 55
+
+	fmt.Println(name)
+	fmt.Println(name_infer)
+
+	fmt.Println(name_two)
+
+	fmt.Println(age)
+}
